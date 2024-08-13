4881d03989e4fe0021292d9f0f3f37a813eae713
Theo91500 <167212980+Theo91500@users.noreply.github.com>
2024-08-14 01:11:44 +0200

    commentaires ajoutés

--- FrontEnd/assets/js/modals.js
+++ FrontEnd/assets/js/modals.js
@@ -5,21 +5,20 @@ const buttonClose = document.querySelectorAll('.buttonClose');
 const buttonBack = document.querySelector('.buttonBack');
 
 
-
-
 function openModals() {
     for (let i = 0; i < modals.length; i++) {
         modalsButton[i].addEventListener('click', () =>{
 
             if (i == 1) {
-                modals[0].classList.add('displayNone');
+                modals[0].classList.add('displayNone');  //Si on click sur ajouter photo alors la modale 0 se voit appliquer le style display none et donc disparait
             }
 
-            modals[i].classList.remove('displayNone');
-            filter.classList.remove('displayNone');
+            modals[i].classList.remove('displayNone'); // Après avoir cliqué sur ce boutton, la modale 1 voit son style displaynone retiré et donc elle apparait
+            filter.classList.remove('displayNone'); //ca fait apparaitre le filtre
         });
     }
 }
+
 
 function closeModals() {
 
@@ -40,7 +39,7 @@ function backModal(){
     for (let i = 0; i < modals.length; i++) {
         buttonBack.addEventListener('click', ()=>{
             modals[1].classList.add('displayNone');
-            modals[0].classList.remove('displayNone');
+            modals[0].classList.remove('displayNone'); // Si on fait retour ca remet le style display none sur la modale 1 (pour la faire disparaitre) 
         });
     }
 
